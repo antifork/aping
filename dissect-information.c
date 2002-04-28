ba953f91a3699bb3c94450489903de193d0edbef
awgn <awgn@antifork.org>
2002-04-28 01:57:22 +0000

    information request/reply

--- dissect-information.c
+++ dissect-information.c
@@ -1,4 +1,49 @@
-/* ICMP_INFO_REQUEST */
+/*
+ * $Id$
+ * 
+ * New aping.
+ * 
+ * Copyright (c) 2002 Nicola Bonelli <bonelli@antifork.org>
+ *                    Roberto Ferranti <sbirish@sbirish.net>
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer. 2.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * 
+ * 
+ */
+
+/* ICMP_INFO_REQUEST 
+
+   Information Request or Information Reply Message
+
+    0                   1                   2                   3
+    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |     Type      |      Code     |          Checksum             |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |           Identifier          |        Sequence Number        |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+*/
  
 #include "dissect.h" 
 #include "aping.h"
@@ -10,9 +55,17 @@
 void
 load_information_request(packet *p, char **argv)
 {
+  ICMP_type(p)= ICMP_INFO_REQUEST;
+  ICMP_code(p)= 0;
+  ICMP_id(p)  = myid;
+  ICMP_seq(p) = n_sent;
+
+  ICMP_sum(p)= 0;
+  ICMP_sum(p)= chksum((u_short *)p->icmp, sizeof_icmp(ICMP_ECHO));
 }
 
 void
 dissect_information_request(packet *p)
 {
+  PUTS("icmp_ud=%ld seq=%ld\n",ICMP_id(p), ICMP_seq(p));
 }
