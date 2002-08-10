4d016bf2cdfc8d6d449d33e64e056efa2f02d774
awgn <awgn@antifork.org>
2002-08-10 18:49:52 +0000

    *** empty log message ***

--- pcap-stub.h
+++ pcap-stub.h
@@ -3,9 +3,9 @@
 #ifndef STUB_PCAP_H
 #define STUB_PCAP_H
  
-#ifdef HAVE_PCAP_H
+#if defined(HAVE_PCAP_H)
 #include <pcap.h>
-#elif HAVE_PCAP_PCAP_H
+#elif defined(HAVE_PCAP_PCAP_H)
 #include <pcap/pcap.h>
 #else
 #error "pcap.h header not installed"
